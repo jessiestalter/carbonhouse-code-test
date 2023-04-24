69f8001e2fb265bedf17c7604ea614635c038d09
jessiestalter <jrstalter11@gmail.com>
2023-04-24 18:33:01 -0400

    Finalizing files & deleting unused images

--- js/navBar.js
+++ js/navBar.js
@@ -1,8 +1,5 @@
 /* navBar.js */
 /* Implements hover styling for nav bar links */
-/* Implements navBar scroll down functionality */
-
-/* Nav bar link hover styling */
 
 let navLinks = $('.navLink');
 
@@ -14,15 +11,3 @@ navLinks.mouseover(function(event) {
 navLinks.mouseout(function() {
     navLinks.removeClass('inactive');
 });
-
-/* Nav bar scroll down functionality */
-// var prevPosition = window.pageYOffset;
-// window.onscroll = function() {
-//   var currPosition = window.pageYOffset;
-//   if (prevPosition < currPosition) {
-//     $('#navBar').addClass('topGone');
-//   } else {
-//     $('#navBar').removeClass('topGone');
-//   }
-//   prevPosition = currPosition;
-// }
